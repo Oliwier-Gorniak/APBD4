3049833b810773bbdd0428c2057474adabf6c75b
Oliwier-Gorniak <olioli7752@gmail.com>
2024-04-08 21:16:24 +0200

    Created Interfaces

---
+++ LegacyApp/IUserCreditService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LegacyApp;
+
+public interface IUserCreditService
+{
+    decimal GetCreditLimit(string lastName, DateTime dateOfBirth);
+}
---
+++ LegacyApp/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace LegacyApp;
+
+public class UserRepository : IUserRepository
+{
+    private readonly List<User> users;
+
+    public UserRepository()
+    {
+        users = new List<User>();
+    }
+
+    public void AddUser(User user)
+    {
+        users.Add(user);
+    }
+}
